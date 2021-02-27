a6cfc8070eca64779871a2548d6d566b23903b14
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-02-27 16:05:15 +0000

    feat: Add Countdown component

---
+++ src/components/Countdown/index.tsx
@@ -0,0 +1,64 @@
+import { useCountDown } from "contexts/Countdown";
+
+import { Container, CountdownButton } from "./styles";
+
+const Countdown = () => {
+    const {
+        minutes,
+        seconds,
+        hasFinished,
+        isActive,
+        startCountdown,
+        resetCountdown,
+    } = useCountDown();
+
+    const [minutesLeft, minutesRight] = `0${minutes}`.slice(-2).split("");
+    const [secondsLeft, secondsRight] = `0${seconds}`.slice(-2).split("");
+
+    return (
+        <>
+            <Container>
+                <div>
+                    <span>{minutesLeft}</span>
+                    <span>{minutesRight}</span>
+                </div>
+
+                <span>:</span>
+
+                <div>
+                    <span>{secondsLeft}</span>
+                    <span>{secondsRight}</span>
+                </div>
+            </Container>
+
+            {hasFinished ? (
+                <CountdownButton disabled active={isActive}>
+                    Ciclo encerrado
+                    <img src="icons/check-circle.svg" alt="Completado" />
+                </CountdownButton>
+            ) : (
+                <>
+                    {isActive ? (
+                        <CountdownButton
+                            active={isActive}
+                            onClick={resetCountdown}
+                        >
+                            Abandonar ciclo
+                            <img src="icons/close.svg" alt="Abandonar" />
+                        </CountdownButton>
+                    ) : (
+                        <CountdownButton
+                            active={isActive}
+                            onClick={startCountdown}
+                        >
+                            Iniciar um ciclo
+                            <img src="icons/play-arrow.svg" alt="Iniciar" />
+                        </CountdownButton>
+                    )}
+                </>
+            )}
+        </>
+    );
+};
+
+export default Countdown;
