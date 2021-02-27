f6bc135e3a25967e3e6a6a6e5452f7d212604285
Marcelo Fortunato <marcelo.fortunato@farfetch.com>
2021-02-27 16:07:07 +0000

    feat: Add Profile component

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,26 @@
+import { useChallenges } from 'contexts/Challenges'
+
+import { Container } from './styles'
+
+const Profile = () => {
+    const { level } = useChallenges()
+
+    return (
+        <Container>
+            <img
+                src="https://github.com/marcelof5.png"
+                alt="Marcelo Fortunato Profile"
+            />
+
+            <div>
+                <strong>Marcelo Fortunato</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
+                </p>
+            </div>
+        </Container>
+    )
+}
+
+export default Profile
